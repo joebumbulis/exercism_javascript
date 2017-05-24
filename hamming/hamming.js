b83665840dabd1495226cc84ed16b6952695b7ee
Joe Bumbulis <joebumbulis@gmail.com>
2017-05-24 18:53:15 -0500

    solved basic problem for hamming

--- hamming/hamming.js
+++ hamming/hamming.js
@@ -1,7 +1,14 @@
-var Hamming = function(input) {
-  this.value = input;
+var Hamming = function(string1, string2) {
+  this.value = string1;
+  this.valueTwo = string2;
 };
 
-Hamming.prototype.compute = function() {};
+Hamming.prototype.compute = function(string1, string2) {
+  if (string1 !== string2) {
+    return 1;
+  } else if (string1 === string2) {
+    return 0;
+  }
+};
 
 module.exports = Hamming;
