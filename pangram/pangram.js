a7da89fd417df52a512e38710a9f171396dd5d58
Joe Bumbulis <joebumbulis@gmail.com>
2017-05-30 15:11:55 -0500

    solving first test with if/else and for statements

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -1,7 +1,15 @@
-var Pangram = function(sentence) {
-  this.value = sentence;
-};
+var Pangram = function(str) {};
 
-Pangram.prototype.isPangram = function() {};
+Pangram.prototype.isPangram = function(str) {
+  if (typeof str !== "string") {
+    return false;
+  }
+  for (var i = 0; i < str.length; i++) {
+    if (str.charCodeAt(i) > 127) {
+      return false;
+    }
+  }
+  return true;
+};
 
 module.exports = Pangram;
