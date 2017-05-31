75db8865c0f73ba46bc8c517984c50ea05f44ff0
Joe Bumbulis <joebumbulis@gmail.com>
2017-05-30 20:40:10 -0500

    completed pangram, no failures in tests

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -1,23 +1,3 @@
-// var Pangram = function() {};
-//
-// Pangram.prototype.isPangram = function(str) {
-//   if (typeof str !== "string") {
-//     return false;
-//   }
-//   for (var i = 0; i < str.length; i++) {
-//     if (str.charCodeAt(i) > 65 < 91 > 127) {
-//       return false;
-//     }
-//     // else if (str.charCodeAt(i) < 91) {
-//     //   return false;
-//     // }
-//   }
-//   return true;
-// };
-//
-// module.exports = Pangram;
-
-///
 // var Pangram = function(sentence) {
 //   this.sentence = sentence.toLowerCase().split("");
 //   this.metric = {
