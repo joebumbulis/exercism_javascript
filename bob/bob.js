904548c51a0c470243a0271d5b50186f4236f611
Joe Bumbulis <joebumbulis@gmail.com>
2017-06-01 13:28:41 -0500

    completed first test, no failures

--- bob/bob.js
+++ bob/bob.js
@@ -1,7 +1,11 @@
-var Bob = function() {};
+var Bob = function(msg) {
+  this.msg = msg;
+};
 
 Bob.prototype.hey = function(msg) {
   switch (msg) {
   }
   return "Whatever.";
 };
+
+module.exports = Bob;
